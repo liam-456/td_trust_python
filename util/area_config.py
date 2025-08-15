20ecf992443b05a128deb72f99ec5aa73293dd0a
Liam Guermache <60001391+liam-456@users.noreply.github.com>
2025-08-15 18:18:22 +0100

    Update area_config.py
    add north london line

--- util/area_config.py
+++ util/area_config.py
@@ -2,5 +2,6 @@ NAMED_AREAS = {
     "tottenham": ["Q1", "Q3", "Q4"],
     "thameslink_core": ["X0"],
     "ecml_south": ["Y9", "Y8"],
+    "nll": ["AW", "Q7", "WI", "XA"],
     "all": []  # Special case to include all areas
 }
